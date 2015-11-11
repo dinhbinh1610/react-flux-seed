0b801ed9ea47f4ce6a178eeec339f8959d2b340a
Corey Prophitt <prophitt.corey@gmail.com>
2015-11-11 00:35:27 -0800

    Added some basic home logic

--- app/js/components/home.js
+++ app/js/components/home.js
@@ -5,28 +5,51 @@
  */
 var React = require("react");
 
+/*
+ * "Private" variables go here
+ */
+var _countDownTicker;
+
 var Home = React.createClass({
 
     componentDidMount: function () {
-        /*
-         * Do nothing right now...
-         * */
+        _countDownTicker = setInterval(this._onTick, 1000);
     },
 
     componentWillUnmount: function () {
-        /*
-         * Do nothing right now...
-         * */
+        if (_countDownTicker) {
+            clearInterval(_countDownTicker);
+        }
+    },
+
+    getInitialState: function () {
+        return {
+            ticksRemaining: 10
+        };
     },
 
     render: function () {
+        var displayText = "Action Triggered!";
+
+        if (this.state.ticksRemaining > 0) {
+            displayText = this.state.ticksRemaining + "...";
+        }
+
         return (
             <div>
                 <span className="hello-react">
-                    Hello, React!
+                    {displayText}
                 </span>
             </div>
         );
+    },
+
+    _onTick: function () {
+        if (this.state.ticksRemaining > 0) {
+            this.setState({
+                ticksRemaining: this.state.ticksRemaining - 1
+            });
+        }
     },
 
 });
